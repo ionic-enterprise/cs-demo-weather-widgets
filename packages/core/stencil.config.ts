fd12d9d8250836f1b6a7efff997cbf7c1d6fe865
Ken Sodemann <ken@ionic.io>
2022-04-22 12:59:43 -0500

    feat(react): add the React framweork wrapper

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,8 +1,15 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget as react } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'csdemoweather',
   outputTargets: [
+    react({
+      componentCorePackage: '@ionic-enterprise/cs-demo-weather-widgets',
+      proxiesFile: '../react/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: false,
+      includeImportCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
---
+++ packages/react/README.md
@@ -0,0 +1,3 @@
+# Framework Wrapper: React
+
+TODO: see the game-dots, fill this out in a similar manner, include a usage section
